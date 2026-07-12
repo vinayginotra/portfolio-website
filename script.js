788c4abde862971628a0048dc5e3b7c8543a8980
vinayginotra <vinayginotra@gmail.com>
2026-07-13 00:33:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -198,3 +198,31 @@ card.addEventListener("mouseleave",()=>{
 card.style.transform="rotateX(0deg) rotateY(0deg)";
 
 });
+const card = document.querySelector(".profile-card");
+
+if(card){
+
+card.addEventListener("mousemove",(e)=>{
+
+const r = card.getBoundingClientRect();
+
+const x = e.clientX-r.left;
+
+const y = e.clientY-r.top;
+
+const rx = (y-r.height/2)/18;
+
+const ry = (x-r.width/2)/18;
+
+card.style.transform =
+`rotateX(${-rx}deg) rotateY(${ry}deg)`;
+
+});
+
+card.addEventListener("mouseleave",()=>{
+
+card.style.transform="rotateX(0deg) rotateY(0deg)";
+
+});
+
+}
